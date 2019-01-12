1105e14b6aa7a9f128b911936edcbaab5def0617
EFastner <eric.fastner@gmail.com>
2019-01-12 17:23:50 -0600

    Added plot

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -3,6 +3,14 @@ server <- function(input, output) {
   df.all_summaries <- read_csv("all_seasons.csv", col_names = TRUE)
   df.lock_line <- read_csv("lock_line.csv")
   
+  #Filter dataframe based on criteria
+  df.team_results <- reactive({
+    df.all_summaries %>%
+      filter(team == input$teamInput,
+             season == input$seasonInput,
+             session == "R")
+  })
+    
   #Dynamic UI to Select all Teams in Dataset
   output$teamOutput <- renderUI({
     selectInput(inputId = "teamInput",
@@ -19,6 +27,34 @@ server <- function(input, output) {
                   filter(df.all_summaries, 
                          team == input$teamInput)$season)),
                 selected = "20172018")
+  })
+  
+  output$viz.lock_line <- renderPlot({
+    #Create the labels for the plot
+    df.line_labels <- reactive({
+      df.team_results() %>% 
+        dplyr::group_by(team, season) %>%
+        dplyr::summarise(points = max(team_point_total),
+                         games = max(team_game)) %>%
+        dplyr::mutate(label = paste0(team, "-", season, "\n", points, " Points"))
+    })
+    
+    #Create Viz
+    viz.lockline_performance <- 
+      ggplot() +
+      ggtitle("Lock Line and Point of No Return") +
+      theme(plot.title = element_text(hjust = 0.5), legend.position = "none") +
+      labs(x = "Game Number", y = "Total Points", color = "Team") +
+      geom_line(data = df.lock_line, na.rm = TRUE, mapping = aes(x = team_game, y = lock_line), color = "blue", linetype = "longdash", alpha = .3) +
+      geom_line(data = df.lock_line, na.rm = TRUE, mapping = aes(x = team_game, y = no_return), color = "red", linetype = "longdash", alpha = .3) +
+      geom_line(data = df.team_results(), mapping = aes(x = team_game, y = team_point_total, color = team)) +
+      geom_text(data = df.line_labels(), mapping = aes(x = games, y = points, label = label, color = team), nudge_x = 1.75)
+    
+    viz.lockline_performance
+  })
+  
+  output$df.results_table <- renderTable({
+
   })
   
 }
--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -1,14 +1,18 @@
 library(readr)
 library(plyr)
 library(dplyr)
+library(ggplot2)
+library(ggrepel)
 
 ui <- fluidPage(
   titlePanel("Team Performance vs Lock Line", 
              windowTitle = "Lock Line Analysis"),
   sidebarLayout(
+    sidebarPanel(
     uiOutput("teamOutput"),
     uiOutput("seasonOutput")),
-  mainPanel(plotOutput("viz.lock_line"),
-            br(), br(),
-            tableOutput("df.team_results"))
+    mainPanel(plotOutput("viz.lock_line"),
+              br(), br(),
+              tableOutput("df.results_table"))
+  )
 )
